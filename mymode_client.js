87bdd77a702fc607b507fdd851207496dfac20d7
Daif <dev.daif@gmail.com>
2024-05-08 23:08:38 -0400

    t

--- mymode_client.js
+++ mymode_client.js
@@ -18,7 +18,7 @@ on('onClientGameTypeStart', () => {
   exports.spawnmanager.forceRespawn()
 });
 
-Delay = (ms) => new Promise(res => setTimeout(res, ms));
+const Delay = (ms) => new Promise(res => setTimeout(res, ms));
 
 RegisterCommand('gerogay', async (source, args) => {
   let model = "adder";
@@ -26,6 +26,7 @@ RegisterCommand('gerogay', async (source, args) => {
     model = args[0].toString();
   }
 
+  // check if the model actually exists
   const hash = GetHashKey(model);
   if (!IsModelInCdimage(hash) || !IsModelAVehicle(hash)) {
     emit('chat:addMessage', {
@@ -34,25 +35,29 @@ RegisterCommand('gerogay', async (source, args) => {
     return;
   }
 
+  // Request the model and wait until the game has loaded it
   RequestModel(hash);
   while (!HasModelLoaded(hash)) {
     await Delay(500);
   }
 
-  const playerPed = PlayerPedId()
-  const [playerX, playerY, playerZ] = GetEntityCoords(playerPed);
+  const ped = PlayerPedId();
 
+  // Get the coordinates of the player's Ped (their character)
+  const coords = GetEntityCoords(ped);
 
-  const vehicle = CreateVehicle(vehicleName, playerX, playerY, playerZ, GetEntityHeading(playerPed), true, true)
+  // Create a vehicle at the player's position
+  const vehicle = CreateVehicle(hash, coords[0], coords[1], coords[2], GetEntityHeading(ped), true, false);
 
-  SetPedIntoVehicle(playerPed, vehicle, -1)
+  // Set the player into the drivers seat of the vehicle
+  SetPedIntoVehicle(ped, vehicle, -1);
 
-  SetEntityAsNoLongerNeeded(vehicle)
-  SetModelAsNoLongerNeeded(vehicleName)
+  // Allow the game engine to clean up the vehicle and model if needed
+  SetEntityAsNoLongerNeeded(vehicle);
+  SetModelAsNoLongerNeeded(model);
 
+  // Tell the player the car spawned
   emit('chat:addMessage', {
-    args: [
-      'Spawned ' + vehicleName
-    ]
-  })
+    args: [`Woohoo! Enjoy your new ^*${model}!`]
+  });
 }, false)
